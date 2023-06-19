65f02d443fa57b6a81d8bf130b4157501c8a3771
bruno <brunoqueiroz612@gmail.com>
2023-06-19 16:49:39 -0300

    Create LinkIcon component

---
+++ src/components/LinkIcon.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+interface LinkIconProps {
+  children: ReactNode;
+  href: string;
+}
+
+const LinkIcon = ({ href, children }: LinkIconProps) => {
+  return (
+    <a
+      href={href}
+      className="grid place-items-center z-10 border-primary border-[3px] p-2 rounded-full relative link-icon-hover"
+    >
+      {children}
+    </a>
+  );
+};
+
+export default LinkIcon;
