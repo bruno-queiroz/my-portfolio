62886f362e65411de924de4e1e6f55fb77158d98
bruno <brunoqueiroz612@gmail.com>
2023-06-19 16:19:50 -0300

    Add icons to the Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,14 @@
+import { BsGithub as Githubicon } from "react-icons/bs";
+
+import { FaLinkedinIn as LinkInIcon } from "react-icons/fa";
+
 const Hero = () => {
   return (
     <section className="flex justify-center p-4">
       <div className="flex flex-col max-w-[1200px] w-full mt-4">
         <span>Hi my name is</span>
 
-        <h1 className="flex flex-col gap-2 font-bold text-[4rem] leading-[4rem]">
+        <h1 className="flex flex-col gap-2 text-primary font-bold text-[4rem] leading-[4rem]">
           <span>Bruno Queiroz</span>
           <span>I do Web Devolopment</span>
         </h1>
@@ -16,7 +20,16 @@ const Hero = () => {
           beatae alias similique nostrum!
         </p>
 
-        <button className="w-[max-content] border-red-400 border-[3px] p-2 rounded mt-8">
+        <div className="flex gap-2 text-lg mt-4">
+          <a href="" className="border-white border-[3px] p-2 rounded-full">
+            <Githubicon />
+          </a>
+          <a href="" className="border-white border-[3px] p-2 rounded-full">
+            <LinkInIcon />
+          </a>
+        </div>
+
+        <button className="w-[max-content] border-primary border-[3px] p-2 rounded mt-8">
           Reach out
         </button>
       </div>
