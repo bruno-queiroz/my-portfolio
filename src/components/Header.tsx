e3b9d96b79cafc0957a10d12dfa6f6614c606d0e
bruno <brunoqueiroz612@gmail.com>
2023-06-20 15:44:16 -0300

    Add border to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <div className="flex justify-center bg-red-100 px-4 py-6">
-      <header className="flex justify-between p-4 rounded-[100vh] bg-red-300 w-[95%]">
+    <div className="flex justify-center px-4 py-6">
+      <header className="flex justify-between p-4 rounded-[100vh] w-full border-transparent border-[3px] border-l-primary border-l-[3px] border-r-primary border-r-[3px]">
         <a href="#" className="font-semibold text-lg">
-          Bruno Queiroz
+          {"<Bruno Queiroz />"}
         </a>
 
         <nav>
