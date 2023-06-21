b361177415460b4c81228e12fd8c3f4db6cb38a4
bruno <brunoqueiroz612@gmail.com>
2023-06-21 17:11:18 -0300

    Create technologies content

---
+++ src/content/technologies.ts
@@ -0,0 +1,52 @@
+import {
+  SiTypescript as TypescriptLogo,
+  SiMongodb as MongoLogo,
+  SiPostgresql as PostgresLogo,
+} from "react-icons/si";
+import { IoLogoCss3 as CssLogo, IoLogoHtml5 as HtmlLogo } from "react-icons/io";
+import { FaReact as ReactLogo, FaNodeJs as NodeLogo } from "react-icons/fa";
+
+export const technologies = [
+  {
+    name: "Typescript",
+    color: "#007acc",
+    Icon: TypescriptLogo,
+    ex: 90,
+  },
+  {
+    name: "HTML",
+    color: "#e34c26",
+    Icon: HtmlLogo,
+    ex: 87,
+  },
+  {
+    name: "CSS",
+    color: "#2965f1",
+    Icon: CssLogo,
+    ex: 84,
+  },
+  {
+    name: "React",
+    color: "#88dded",
+    Icon: ReactLogo,
+    ex: 81,
+  },
+  {
+    name: "NodeJs",
+    color: "#3c873a",
+    Icon: NodeLogo,
+    ex: 78,
+  },
+  {
+    name: "MongoDB",
+    color: "#4DB33D",
+    Icon: MongoLogo,
+    ex: 75,
+  },
+  {
+    name: "PostgreSQL",
+    color: "#0064a5",
+    Icon: PostgresLogo,
+    ex: 35,
+  },
+];
