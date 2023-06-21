20298b12167c4be8c7f43ff9b13ea8fb1e953e47
bruno <brunoqueiroz612@gmail.com>
2023-06-21 17:10:02 -0300

    Create TechContainer component

---
+++ src/components/TechContainer.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import { IconType } from "react-icons";
+
+interface TechContainerProps {
+  name: string;
+  color: string;
+  Icon: IconType;
+  ex: number;
+}
+
+const TechContainer = ({ Icon, name, color, ex }: TechContainerProps) => {
+  return (
+    <div
+      className="flex gap-4 items-center p-4 rounded-lg bg-black/25 border-[2px]"
+      style={{ width: ex + "%", borderColor: color }}
+    >
+      <Icon className="text-2xl" style={{ color }} />
+      <span>{name}</span>
+    </div>
+  );
+};
+
+export default TechContainer;
