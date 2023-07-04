8e2a21ecf176cf4c76958c9bb95289fdf1dfee3d
bruno <brunoqueiroz612@gmail.com>
2023-07-04 15:13:19 -0300

    Add glowy shadow to button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,19 @@
 import { BsGithub as Githubicon } from "react-icons/bs";
-
 import { FaLinkedinIn as LinkInIcon } from "react-icons/fa";
+
 import LinkIcon from "./LinkIcon";
 
 const Hero = () => {
   return (
-    <section className="flex justify-center p-4">
+    <section className="flex justify-center p-4 hero-height">
       <div className="flex flex-col max-w-[1200px] w-full mt-4">
         <span className="text-primary font-semibold">Hi my name is</span>
 
         <h1 className="flex flex-col gap-2 font-bold text-[4rem] leading-[4rem]">
           <span className="text-gray-300">Bruno Queiroz</span>
-          <span className="text-gray-300">I do Web Devolopment</span>
+          <span className="text-gray-300">
+            I do Web Devolopment<span className="text-primary">.</span>
+          </span>
         </h1>
 
         <p className="max-w-[600px] mt-8">
@@ -31,7 +33,7 @@ const Hero = () => {
           </LinkIcon>
         </div>
 
-        <button className="w-[max-content] border-primary border-[3px] p-2 rounded mt-8 test text-primary test2">
+        <button className="w-[max-content] border-primary border-[3px] p-2 rounded mt-8 text-glowy-shadow text-primary box-glowy-shadow">
           Reach out
         </button>
       </div>
