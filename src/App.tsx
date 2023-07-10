f5928d593f4f69c41f64cdd43e1b94e950ec9a29
bruno <brunoqueiroz612@gmail.com>
2023-07-10 15:19:41 -0300

    Add Contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { About } from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Contact />
       <Footer />
     </>
   );
