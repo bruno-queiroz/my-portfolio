c4e23d89bab03543554ce23af90af7519a86f7aa
bruno <brunoqueiroz612@gmail.com>
2023-07-10 15:13:41 -0300

    Create LoadingSpinner component

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div>
+      <div className="w-[23px] h-[23px] rounded-full animate-spin border-[4px] border-transparent border-t-white" />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
