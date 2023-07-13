c7c01bfcfbead746598171229a008caf0c31c212
bruno <brunoqueiroz612@gmail.com>
2023-07-13 07:16:58 -0300

    Change TechMiniCard bg color

--- src/components/TechMiniCard.tsx
+++ src/components/TechMiniCard.tsx
@@ -9,12 +9,12 @@ interface TechMiniCardProps {
 const TechMiniCard = ({ color, Icon, name }: TechMiniCardProps) => {
   return (
     <div className="flex" id="icon" style={{ color }}>
-      <div className="p-2 bg-black/70 rounded-tl-md rounded-bl-md">
+      <div className="p-2 bg-black/30 rounded-tl-md rounded-bl-md">
         <Icon className="text-[24px]" />
       </div>
       <div
         id="icon-name-container"
-        className="grid place-items-center duration-300 origin-left transition-all rounded-tr-md rounded-br-md text-white bg-black/70"
+        className="grid place-items-center duration-300 origin-left transition-all rounded-tr-md rounded-br-md text-white bg-black/30"
       >
         <span id="icon-name" className="whitespace-pre-wrap">
           {name}
