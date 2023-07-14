3395b5f037eeb9fc3d6cd436da16eef01f578328
bruno <brunoqueiroz612@gmail.com>
2023-07-14 08:24:15 -0300

    Increase Subtitle's margin bottom

--- src/components/Subtitle.tsx
+++ src/components/Subtitle.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Subtitle = ({ children }: { children: ReactNode }) => {
   return (
-    <h2 className="font-semibold text-4xl text-center mb-4">{children}</h2>
+    <h2 className="font-semibold text-4xl text-center mb-8">{children}</h2>
   );
 };
 
