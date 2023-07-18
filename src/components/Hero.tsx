03e32e9a6cbac1dacfe5adef0a1342217405b330
bruno <brunoqueiroz612@gmail.com>
2023-07-18 16:49:35 -0300

    Create the Hero presentation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,17 +7,19 @@ const Hero = () => {
   return (
     <section className="flex justify-center p-4 hero-height">
       <div className="flex flex-col max-w-[1350px] w-full mt-[20vh]">
-        <span className="text-primary font-semibold">Hi my name is</span>
+        <span className="text-primary font-semibold">Oi, meu nome é</span>
 
         <h1 className="flex flex-col gap-2 font-bold text-[3rem] leading-[3rem] smm:text-[4rem] smm:leading-[4rem]">
           <span className="text-gray-300">Bruno Queiroz</span>
           <span className="text-gray-300">
-            I do Web Devolopment<span className="text-primary">.</span>
+            E eu faço Web Devolopment<span className="text-primary">.</span>
           </span>
         </h1>
 
         <p className="max-w-[600px] mt-8">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
+          Eu gosto de construir aplicações FullStack, principalmente aquelas que
+          me desafiam, me tiram da minha zona de conforto e me fazem aprender
+          coisas novas.
         </p>
 
         <div className="flex gap-2 text-lg mt-4">
