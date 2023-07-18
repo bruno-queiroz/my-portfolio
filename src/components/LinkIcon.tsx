1538976c5fd7355e7cdc5513fbb3c9a4ca878ae5
bruno <brunoqueiroz612@gmail.com>
2023-07-18 16:43:33 -0300

    Change LinkIcon text color to white when hovered

--- src/components/LinkIcon.tsx
+++ src/components/LinkIcon.tsx
@@ -9,7 +9,7 @@ const LinkIcon = ({ href, children }: LinkIconProps) => {
   return (
     <a
       href={href}
-      className="grid place-items-center z-10 border-primary hover:text-black focus:text-black text-primary border-[3px] p-2 rounded-full relative link-icon-hover"
+      className="grid place-items-center z-10 border-primary hover:text-white focus:text-white text-primary border-[3px] p-2 rounded-full relative link-icon-hover"
     >
       {children}
     </a>
