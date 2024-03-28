31fe372c6bf64d6e7aeca34e16c4e2ee4cbc4381
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-28 00:01:00 -0300

    Add dynamic content for Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,14 +2,20 @@ import { projects } from "../content/projects";
 import ProjectCard from "./ProjectCard";
 import Subtitle from "./Subtitle";
 
+import { projects_en } from "../content/languages/english/projects";
+import { projects_pt } from "../content/languages/portuguese/projects";
+import { useLanguage } from "../context/languageContext";
+
 const Projects = () => {
+  const [languageIndex] = useLanguage();
+  const language = [projects_en, projects_pt];
   return (
     <section className="p-4 max-w-[1350px] mx-auto py-12" id="projects">
       <div>
-        <Subtitle>Projetos</Subtitle>
+        <Subtitle>{language[languageIndex].sub_title}</Subtitle>
         <div className="flex gap-20 flex-col">
           {projects.map((project, i) => (
-            <ProjectCard {...project} key={i} />
+            <ProjectCard {...project} index={i} key={i} />
           ))}
         </div>
       </div>
